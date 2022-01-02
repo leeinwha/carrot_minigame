5b8fde7d1f1c5fa7f6e210a78af225d617fefc02
leeinwha <88917793+leeinwha@users.noreply.github.com>
2022-01-02 17:44:49 +0900

    Refactoring : sound

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 'use strict'
 import PopUp from './popup.js';
 import Field from './field.js';
+import * as sound from './sound.js';
 
 
 const CARROT_COUNT = 20;
@@ -12,13 +13,6 @@ const gameBtn = document.querySelector('.game__button');
 const gameTimer = document.querySelector('.game__timer');
 const gameScore = document.querySelector('.game__score');
 
-
-
-const carrotSound = new Audio('./sound/carrot_pull.mp3');
-const alertSound = new Audio('./sound/alert.wav');
-const bgSound = new Audio('./sound/bg.mp3');
-const bugSound = new Audio('./sound/bug_pull.mp3');
-const winSound = new Audio('./sound/game_win.mp3');
 
 let started = false;
 let score = 0;
@@ -63,7 +57,7 @@ function startGame(){
   showStopButton();
   showTimerAndScore();
   startGameTimer();
-  playSound(bgSound);
+  sound.playBankground();
 }
 
 function stopGame() {
@@ -71,17 +65,17 @@ function stopGame() {
   stopGameTimer();
   hideGameButton();
   gameFinishBanner.showWithText('REPLAY ?');
-  playSound(alertSound);
-  stopSound(bgSound);
+  sound.playAlert();
+  sound.stopBackground();
 }
 
 function finishGame(win) {
   started = false;
   hideGameButton();
   if (win) {
-    playSound(winSound);
-  } else {
-    playSound(bugSound);
+    sound.playWin();
+    } else {
+    sound.playBug();
   }
   stopGameTimer();
   stopSound(bgSound);
@@ -133,14 +127,7 @@ function initGame() {
   gameField.init();
 }
 
-function playSound(sound) {
-  sound.currentTime = 0;
-  sound.play();
-}
 
-function stopSound(sound) {
-  sound.pause();
-}
 
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
